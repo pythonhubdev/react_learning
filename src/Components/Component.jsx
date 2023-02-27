e892187b18a8f0bbfe81fb26833dab8229bafe84
Vetrichelvan <pythonhubdev@gmail.com>
2023-02-27 10:45:10 +0530

    Adding javascript logic inside JSX

--- src/Components/Component.jsx
+++ src/Components/Component.jsx
@@ -3,12 +3,14 @@ import "./Component.css"; // Importing CSS files
 
 const CustomComponent = (props) => {
     const description = "This is a short description";
+    // Adding normal JavaScript logic inside JSX
+    const date = new Date();
     return (
         <div>
             {/* Showing dynamic data via props */}
             <h1 style={{color: "red"}}>{props.title}</h1>
             {/* Showing dynamic data via variable */}
-            <p className="short-description">{description}</p>
+            <p className="short-description">{description} on {date.toDateString()}</p>
         </div>
     );
 }
