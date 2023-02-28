f1d29c5c1770277416f224ea2950a9b2e02549d8
Vetrichelvan <pythonhubdev@gmail.com>
2023-02-28 21:07:44 +0530

    Event handlers and listening to events in react

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import CComponent from "./Components/Component";
 function App() {
     return (
         <div>
-            {/*Wrapping the children component with the CComponent.
-            Since we are using the children prop, we can pass* any component as a child to the CComponent */}
             <CComponent>
                 <ChildrenComponent/>
             </CComponent>
--- src/Components/Component.jsx
+++ src/Components/Component.jsx
@@ -2,12 +2,41 @@ import "./Component.css"; // Importing CSS files
 
 
 const CustomComponent = (props) => {
+
+    const handleClick = () => {
+        // Passing a function to the onClick attribute
+        console.log("Simple click");
+        alert("Simple click");
+    }
+
     return (
         <div>
             <div>
-                {/* Passing children via props: with the help of this,
-                we can pass another HTML component as children */}
                 {props.children}
+                {/*
+                    With the help of JSX syntax,
+                    you will be able to add function to a native HTML element via the custom attributes provided via React
+                */}
+                <button
+                    onClick={
+                        () => {
+                            // Passing an anonymous function to the onClick attribute
+                            console.log("Simple click");
+                            alert("Simple click");
+                        }
+                    }>Anonymous click
+                </button>
+                {/*
+                    When using JSX code, it's important to keep in mind that the code will be parsed before the
+                    browser renders the page. Therefore, it's recommended that you pass the function name as a
+                    reference instead of calling the function directly. This can help improve the efficiency and
+                    performance of your code. By passing the function name, you ensure that the function is only
+                    called when it's actually needed, rather than being called unnecessarily during the parsing stage.
+                 */}
+                <button
+
+                    onClick={handleClick}>Function Block Click
+                </button>
             </div>
         </div>
     );
